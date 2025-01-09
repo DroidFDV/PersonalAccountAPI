49651ec0d38e306149988169e63bf12ca9045d4a
DroidFDV <droid.world@yandex.ru>
2025-01-09 14:54:48 +0300

    Added POST localhost:8080/login

--- main.go
+++ main.go
@@ -2,26 +2,35 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
-var user struct {
-	id int 
-	login string 
-	password string 
+type user struct {
+	id 			int 	`json:"id"`
+	login 		string 	`json:"login"`
+	password 	string 	`json:"password"`
 }
 
 var users = []user{
 	{id: 1, login: "droid", password: "1"},
-	{id: 2, login: "mtvy", password: "2"}
+	{id: 2, login: "mtvy", password: "2"},
 }
 
 func main() {
-	
 	router := gin.Default()
 
-	// Определяем маршрут и обработчик
-	router.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello, World!")
+	router.POST("/login", func(c *gin.Context) {
+		login := c.Query("login")
+		password := c.Query("password")
+		// для таких целей лучше использоывать карту, где ключ будет login,
+		// а значение password, но я пока не разбирался
+		for _, user := range users {
+			if user.login == login && user.password == password {
+				c.JSON(http.StatusOK, gin.H{"id": user.id})
+				return
+			}
+		}
+		c.JSON(401, gin.H{"error": "Unauthorized"})
 	})
 
 	// Запускаем сервер на порту 8080
