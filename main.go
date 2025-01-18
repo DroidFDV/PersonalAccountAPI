67ba0fc7f987f0939ddb99087e4e20ebac2745ca
DroidFDV <droid.world@yandex.ru>
2025-01-18 15:03:17 +0300

    Refactoring main.go

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"log/slog"
 	"net/http"
 	"strconv"
 
@@ -36,128 +37,127 @@ var conn *pgx.Conn
 //	rows, err := u.db.Query(...
 //
 // func (u *UserHandle) Login(...
-func getIdByLoginFromDb(cntxt *gin.Context, login, password string) {
-	var id int
 
+func getIDByLoginFromDB(ctx context.Context, login, password string) (int, error) {
 	query := `SELECT id FROM users WHERE login = $1 AND password = $2`
-	rows, err := conn.Query(context.Background(), query, login, password)
+	rows, err := conn.Query(ctx, query, login, password)
 	if err != nil {
-		log.Panic(errors.Wrap(err, "POST /login Query"))
-		rows.Close()
-		return
+		return 0, errors.Wrap(err, "POST /login Query")
 	}
+	defer rows.Close()
 
+	var id int
 	if rows.Next() {
-		err := rows.Scan(&id)
-		if err != nil {
-			log.Panic(errors.Wrap(err, "POST /login Scan"))
-			rows.Close()
-			return
+		if err := rows.Scan(&id); err != nil {
+			return 0, errors.Wrap(err, "POST /login Scan")
 		}
-		cntxt.JSON(http.StatusOK, gin.H{"Id": id})
-		rows.Close()
-		return
+		return id, errors.Wrap(err, "POST /login")
 	}
-	rows.Close()
-
-	cntxt.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+	return id, errors.Wrap(err, "")
 }
 
-func getUserByIdFromDb(cntxt *gin.Context, id int) {
-	var login string
+func getUserByIDFromDB(ctx context.Context, id int) (string, error) {
 
 	query := `SELECT login FROM users WHERE id = $1`
-	rows, err := conn.Query(context.Background(), query, id)
+	rows, err := conn.Query(ctx, query, id)
 	if err != nil {
-		log.Panic(errors.Wrap(err, "GET /user/:id Query\n"))
-		rows.Close()
-		return
+		return "", errors.Wrap(err, "GET /user/:id Query")
 	}
+	defer rows.Close()
 
+	var login string
 	if rows.Next() {
-		err := rows.Scan(&login)
-		if err != nil {
-			log.Panic(errors.Wrap(err, "POST /login Scan"))
-			rows.Close()
-			return
+		if err := rows.Scan(&login); err != nil {
+			return "", errors.Wrap(err, "GET /user/:id Scan")
 		}
-		cntxt.JSON(http.StatusOK, gin.H{"user": login})
-		rows.Close()
-		return
+		return login, errors.Wrap(err, "GET /user/:id")
 	}
-	rows.Close()
-
-	cntxt.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+	return login, errors.Wrap(err, "")
 }
 
-func addingUserToDb(cntxt *gin.Context, id int, login, password string) {
+func addingUserToDB(ctx context.Context, id int, login, password string) error {
 	query := `INSERT INTO users (id, login, password) VALUES ($1, $2, $3)`
-	// был вариант передавать структуру user
-	// _, err := conn.Exec(context.Background(), query, user.Id, user.Login, user.Password)
-	_, err := conn.Exec(context.Background(), query, id, login, password)
-	if err != nil {
-		log.Panic(errors.Wrap(err, "POST /user Exec\n"))
-		return
-	}
-
-	cntxt.JSON(http.StatusOK, gin.H{"user": "added"})
+	_, err := conn.Exec(ctx, query, id, login, password)
+	return errors.Wrap(err, "POST /user Exec")
 }
 
-// доделываю
-func updateUserInDb(cntxt *gin.Context, user user) {
+func updateUserInDB(ctx context.Context, id int, login, password string) error {
 	query := `UPDATE users SET login = $2, password = $3 WHERE id = $1`
-	_, err := conn.Exec(context.Background(), query, user.Id, user.Login, user.Password)
-	if err != nil {
-		log.Panic(errors.Wrap(err, "POST /user Exec\n"))
-		return
-	}
-
-	cntxt.JSON(http.StatusOK, gin.H{"user " + strconv.FormatInt(int64(user.Id), 10): "updated"})
+	_, err := conn.Exec(ctx, query, id, login, password)
+	return errors.Wrap(err, "POST /user Exec")
 }
 
-func loginHandler(cntxt *gin.Context) {
+func loginHandler(c *gin.Context) {
 	var user user
-
-	if err := cntxt.ShouldBind(&user); err != nil {
-		log.Panic(errors.Wrap(err, "POST /login ShouldBind\n"))
+	if err := c.ShouldBind(&user); err != nil {
+		slog.Error(errors.Wrap(err, "POST /login ShouldBind").Error())
 		return
 	}
 
-	getIdByLoginFromDb(cntxt, user.Login, user.Password)
+	id, err := getIDByLoginFromDB(c, user.Login, user.Password)
+	if err != nil {
+		slog.Error(err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Incorrect query"})
+		return
+	}
+	if id == 0 {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"id": id})
 }
 
-func userByIdHandler(cntxt *gin.Context) {
-	idParam := cntxt.Param("id")
+func userByIDHandler(c *gin.Context) {
+	idParam := c.Param("id")
 	id, err := strconv.Atoi(idParam)
 	if err != nil {
-		log.Panic(errors.Wrap(err, "GET /user Atoi\n"))
+		slog.Error(errors.Wrap(err, "GET /user/:id Atoi").Error())
 		return
 	}
 
-	getUserByIdFromDb(cntxt, id)
+	login, err := getUserByIDFromDB(c, id)
+	if err != nil {
+		slog.Error(err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Incorrect query"})
+		return
+	}
+	if login == "" {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"user": login})
 }
 
-func addingHandler(cntxt *gin.Context) {
+func addingHandler(c *gin.Context) {
 	var user user
 
-	if err := cntxt.ShouldBind(&user); err != nil {
-		log.Panic(errors.Wrap(err, "POST /login ShouldBind\n"))
+	if err := c.ShouldBind(&user); err != nil {
+		slog.Error(errors.Wrap(err, "POST /login ShouldBind").Error())
 		return
 	}
 
-	addingUserToDb(cntxt, user.Id, user.Login, user.Password)
+	if err := addingUserToDB(c, user.Id, user.Login, user.Password); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Incorrect user data"})
+		slog.Error(err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"user": "added"})
 }
 
-// доделываю
-func updateHandler(cntxt *gin.Context) {
+func updateHandler(c *gin.Context) {
 	var user user
 
-	if err := cntxt.ShouldBind(&user); err != nil {
-		log.Panic(errors.Wrap(err, "POST /login ShouldBind\n"))
+	if err := c.ShouldBind(&user); err != nil {
+		slog.Error(errors.Wrap(err, "PUT /login ShouldBind").Error())
 		return
 	}
 
-	updateUserInDb(cntxt, user)
+	if err := updateUserInDB(c, user.Id, user.Login, user.Password); err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		slog.Error(err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"user by id: " + strconv.FormatInt(int64(user.Id), 10): "updated"})
 }
 
 func main() {
@@ -167,7 +167,7 @@ func main() {
 	// заменить на conn, err := storage.NewConn(ctx, connString string) (*pgx.Conn, error)
 	conn, err = pgx.Connect(ctx, "postgres://postgres:postgres@localhost:5432/postgres")
 	if err != nil {
-		log.Panic(errors.Wrap(err, "main pgx.Connect\n"))
+		log.Fatal(errors.Wrap(err, "main pgx.Connect"))
 	}
 	defer conn.Close(ctx)
 
@@ -190,7 +190,7 @@ func main() {
 			`
 	_, err = conn.Exec(context.Background(), query)
 	if err != nil {
-		log.Panic(errors.Wrap(err, "Ошибка выполнения запроса CREATE TABLE\n"))
+		log.Panic(errors.Wrap(err, "Ошибка выполнения запроса CREATE TABLE"))
 		return
 	}
 
@@ -201,13 +201,9 @@ func main() {
 	router := gin.Default()
 	// router.POST("/login", handler.Login)
 	router.POST("/login", loginHandler)
-
-	router.GET("/user/:id", userByIdHandler)
-
+	router.GET("/user/:id", userByIDHandler)
 	router.POST("/user", addingHandler)
-
 	router.PUT("/user", updateHandler)
-
 	// оставляй тут
 	router.Run(":8080")
 
