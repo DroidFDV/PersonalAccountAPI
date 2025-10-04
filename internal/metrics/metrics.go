a64f536d29f2945d4e1cd3e18a3f51661be2162f
DroidFDV <droid.world@yandex.ru>
2025-10-04 10:39:43 +0300

    [dev] Added http duration metric. Ref httpRequests

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -2,14 +2,13 @@ package metrics
 
 import (
 	"PersonalAccountAPI/internal/cache"
+	"log/slog"
 	"net/http"
-	"runtime"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/sirupsen/logrus"
 )
 
 var (
@@ -18,14 +17,16 @@ var (
 			Name: "http_requests_total",
 			Help: "Total number of HTTP requests",
 		},
-		[]string{"method", "status"},
+		[]string{"method", "status", "path"},
 	)
 
-	goRoutineCount = prometheus.NewGauge(
-		prometheus.GaugeOpts{
-			Name: "go_routines_count",
-			Help: "Number of active Go routines",
+	httpRequestDuration = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "http_request_duration_seconds",
+			Help:    "Duration of HTTP requests in seconds",
+			Buckets: prometheus.DefBuckets,
 		},
+		[]string{"method", "path"},
 	)
 
 	cacheSize = prometheus.NewGauge(
@@ -36,22 +37,28 @@ var (
 	)
 )
 
-func InitMetrics(port string) {
+func MetricsRegistration() {
 	prometheus.MustRegister(httpRequests)
-	prometheus.MustRegister(goRoutineCount)
 	prometheus.MustRegister(cacheSize)
+	prometheus.MustRegister(httpRequestDuration)
+}
+
+func InitMetrics(port string) {
+	MetricsRegistration()
 	http.Handle("/metrics", promhttp.Handler())
 	go func() {
-		logrus.Printf("Starting metrics server on port %s\n", port)
+		slog.Info("Starting metrics server", slog.String("port", port))
 		if err := http.ListenAndServe(":"+port, nil); err != nil {
-			logrus.Fatalf("Failed to start metrics server: %v", err)
+			slog.Error("InitMetrics http.ListenAndServe", slog.Any("error", err))
 		}
 	}()
 }
 
 func UpdateMetrics(c *cache.CacheDecorator) {
+	ticker := time.NewTicker(1)
+	defer ticker.Stop()
 	for {
-		goRoutineCount.Set(float64(runtime.NumGoroutine()))
+
 		cacheSize.Set(float64(c.GetCacheSize()))
 		time.Sleep(1 * time.Second)
 	}
@@ -59,9 +66,14 @@ func UpdateMetrics(c *cache.CacheDecorator) {
 
 func PrometheusMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		start := time.Now()
 		c.Next()
 		status := c.Writer.Status()
 		method := c.Request.Method
-		httpRequests.WithLabelValues(method, http.StatusText(status)).Inc()
+		path := c.FullPath()
+		duration := time.Since(start).Seconds()
+
+		httpRequests.WithLabelValues(method, http.StatusText(status), path).Inc()
+		httpRequestDuration.WithLabelValues(method, path).Observe(duration)
 	}
 }
